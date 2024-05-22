1d8e9c8d62c2c51bba1d4de1b5e4dcc782c323d8
한경만 <kmhan@spectra.co.kr>
2024-05-22 15:34:28 +0900

    update

--- pdf/app/chat/vector_stores/pinecone.py
+++ pdf/app/chat/vector_stores/pinecone.py
@@ -1,7 +1,10 @@
 import os
 import pinecone
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
+from dotenv import load_dotenv
 from app.chat.embeddings.openai import embeddings
+
+load_dotenv()
 
 pinecone.Pinecone(
     api_key=os.getenv("PINECONE_API_KEY"),
@@ -12,8 +15,10 @@ vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
 
-def build_retriever(chat_args):
-    search_kwargs = {"filter": { "pdf_id": chat_args.pdf_id }}
-    return vector_store.as_retriever(
-        search_kwargs=search_kwargs
-    )
+# vector_store.as_retriever()
+
+# def build_retriever(chat_args):
+#     search_kwargs = {"filter": { "pdf_id": chat_args.pdf_id }}
+#     return vector_store.as_retriever(
+#         search_kwargs=search_kwargs
+#     )
---
+++ pdf/main.py
@@ -0,0 +1,4 @@
+from app.chat import create_embeddings_for_pdf
+
+
+create_embeddings_for_pdf('123456', './경제상식.pdf')
