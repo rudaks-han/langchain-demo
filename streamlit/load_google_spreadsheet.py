a4409e0ab3ca01c136aba9200d371ae8b1d34aca
rudaks <rudaks94@gmail.com>
2024-05-28 22:43:21 +0900

    update

--- streamlit/load_google_spreadsheet.py
+++ streamlit/load_google_spreadsheet.py
@@ -1,12 +1,14 @@
+from dotenv import load_dotenv
 from langchain_google_community import GoogleDriveLoader
 from langchain_community.document_loaders import UnstructuredFileIOLoader
 
+load_dotenv()
 
 loader = GoogleDriveLoader(
     folder_id="1v5ghuHtPR5GnD8qV60_JUhDZFMgKVB3nvN1uI9Tia0U",
     file_types=["sheet"],
     # file_loader_cls=UnstructuredFileIOLoader,
-    # recursive=False,
+    recursive=False,
 )
 
 docs = loader.load()
