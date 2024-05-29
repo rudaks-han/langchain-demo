e4c8deece91b87e6a4a5c517062e49f6455727b0
rudaks <rudaks94@gmail.com>
2024-05-29 23:26:09 +0900

    update

--- demo/retriever/retrieval_qa.py
+++ demo/retriever/retrieval_qa.py
@@ -4,6 +4,7 @@ from langchain_openai.chat_models import ChatOpenAI
 from langchain.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 import langchain
+
 langchain.debug = True
 
 load_dotenv()
@@ -11,19 +12,14 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Retrieving...")
-    chat = ChatOpenAI()
+    llm = ChatOpenAI()
     embeddings = OpenAIEmbeddings()
 
-    db = Chroma(
-        persist_directory="chroma_emb",
-        embedding_function=embeddings
-    )
+    db = Chroma(persist_directory="chroma_emb", embedding_function=embeddings)
 
     retriever = db.as_retriever()
     chain = RetrievalQA.from_chain_type(
-        llm=chat,
-        retriever=retriever,
-        chain_type="stuff"
+        llm=llm, retriever=retriever, chain_type="stuff"
     )
 
     result = chain.invoke("주식 시장이란 무엇인가?")
--- streamlit/load_gspread.py
+++ streamlit/load_gspread.py
@@ -5,17 +5,31 @@ gc = gspread.service_account()
 # https://greeksharifa.github.io/references/2023/04/10/gspread-usage/
 
 gc = gspread.service_account()
-sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1v5ghuHtPR5GnD8qV60_JUhDZFMgKVB3nvN1uI9Tia0U')
+sh = gc.open_by_url(
+    "https://docs.google.com/spreadsheets/d/1v5ghuHtPR5GnD8qV60_JUhDZFMgKVB3nvN1uI9Tia0U"
+)
 
 worksheet = sh.get_worksheet(0)
 
 list_of_lists = worksheet.get_all_values()
 for i, row in enumerate(list_of_lists):
-    if i < 8 or row[8] == '':
+    if i < 9 or row[8] == "":
         continue
     # print(row)
-    score_row = row[8:13]
-    print(score_row)
+    score_row = row[8:14]
+    date = row[8]
+    loser = row[9]
+    loser_score = row[10]
+    money = row[11]
+    winner = row[12]
+    winner_score = row[13]
+
+    print(
+        f"날짜: {date}, 패자팀: {loser}, 패자팀 점수: {loser_score}점, 패자팀 벌금: {money}원, 승자팀: {winner}, 승자팀 점수: {winner_score}점"
+    )
+
+    # ['2024.05.13', '한경만, 허동혁', '4', '₩1,000', '김상현, 김지훈', '11']
+    # print(score_row)
     # for col in row:
     #     print(f"__ {col}")
 
